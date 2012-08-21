aa6bdbec6267cb01a550f86a9b07068c9019d0e1
Tanguy Martin <martin_tanguy@yahoo.fr>
2012-08-21 22:51:27 +0100

    I have no idea what this commit is about

--- js/script.js
+++ js/script.js
@@ -3,25 +3,71 @@
 		i = 0,
 		maxHeight = window.innerHeight,
 		maxWidth = window.innerWidth,
-		getPos = function() {
+		/**
+		 * Return a random position for the elements, within the screen.
+		 * 
+		 * @return {Object}
+		**/
+		getPos = function () {
 			return {
-				left: (Math.random() * (maxWidth - 10) + 10) + 'px',
-				top: (Math.random() * (maxHeight - 10) + 10) + 'px'
+				left: (Math.random() * (maxWidth - 60)) + 'px',
+				top: (Math.random() * (maxHeight - 80)) + 'px'
 			};
 		},
-		pos;
-	while (i < 100) {
+		pos,
+		creationInterval = 100,
+		maxElements = 20;
+
+	// create new element every at a specified interval
+	var intervalId = setInterval(function () {
 		pos = getPos();
+		
 		frag = document.createElement('div');
 		frag.className = "enigma";
+		frag.id = "enigma" + i;
 		frag.innerHTML = '?';
 		frag.style.top = pos.top;
 		frag.style.left = pos.left;
 		document.body.appendChild(frag);
 		i++;
-	}
 
-$('.enigma').on('webkitAnimationIteration mozAnimationEnd msAnimationEnd oAnimationEnd animationIteration', function(e) {
-    //$(this).css({left: getPos().left },0);
-});
+		if (i == maxElements) {
+			clearInterval(intervalId);
+		}
+		
+	}, creationInterval),
+		/**
+		 * Just a simple jQuery cache, 
+		 * the cache id is the id attribute of the DOM element
+		 *
+		**/
+		_$ = (function () {
+
+			var arr = {};
+
+			return function (el) {
+				if (!arr[el.id]) {
+					arr[el.id] = $(el);
+				}
+				return arr[el.id];
+			};
+
+		}());
+
+	$('body').on('webkitAnimationIteration mozAnimationEnd msAnimationEnd oAnimationEnd animationiteration', '.enigma', function(e) {
+		// move at each iteration
+		_$(this).css({
+			left: getPos().left,
+			top: getPos().top
+		});
+	});
+
+	(function(window) {
+		// adjust maxWidth and maxHeight on resize
+		$(window).on('resize', function() {
+			maxHeight = window.innerHeight;
+			maxWidth = window.innerWidth;
+		});
+	}(window));	
+
 }());
